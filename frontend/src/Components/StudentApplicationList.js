0b8b71a237f685129cdd67df8cbc2f0f6d3d01f5
KamaliHari <kamalihari2002@gmail.com>
2023-12-18 00:18:52 +0530

    Schools table.. application part done

--- frontend/src/Components/StudentApplicationList.js
+++ frontend/src/Components/StudentApplicationList.js
@@ -1,81 +1,109 @@
-import React, { useState } from 'react';
-import { Modal } from 'antd';
-import '../Styles/Home.css';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+import { Modal, Button } from 'antd';
 
-export default function StudentApplicationList() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-  const handleCancel = () => {
-    setIsModalOpen(false); }
-    return(
-        <div>
-        <div className="StudentsApplication">
-        <h2 style={{ marginLeft: '10%' , marginTop:'7%'}}>Students's Application</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>S.no</th>
-              <th>Name</th>
-              <th>Locality</th>
-              <th>Additonal Details</th>
-  
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Chokkalingam</td>
-              <td>Madurai, TamilNadu</td>
-              <td style={{ textAlign: 'center' }} >
-              <button type="primary" onClick={showModal}>
-            Document
-          </button>
-          <Modal title="Student Details" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <p>Name: C </p>
-            <p>Degree:</p>
-            <p>Experience:</p>
-          </Modal>
-              </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Madhumitha</td>
-              <td>Jaipur, Rajasthan</td>
-              <td style={{ textAlign: 'center' }}>
-                <button type="primary" onClick={showModal}>
-            Document
-          </button>
-          <Modal title="Student Details" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-          <p>Name: </p>
-          <p>Degree:</p>
-          <p>Experience:</p>
-          </Modal>
-              </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Kamali</td>
-              <td>Tiruvallur, TamilNadu</td>
-              <td style={{ textAlign: 'center' }}>
-              <button type="primary" onClick={showModal}>
-            Document
-          </button>
-          <Modal title="Student Details" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-          <p>Name: </p>
-          <p>Degree:</p>
-          <p>Experience:</p>
-          </Modal>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        </div>
-        </div>
+const DisplayStudentApplications = () => {
+  const [studentApplications, setStudentApplications] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedStudent, setSelectedStudent] = useState(null);
 
-    )
-}
+  useEffect(() => {
+    const fetchStudentApplications = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'StudentApplication'));
+
+        const studentData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          Name: doc.data().Name || '',
+          DisabilityType: doc.data().DisabilityType || '',
+          UDID: doc.data().UDID || '',
+          // Add other fields as needed
+          AadharCardNumber: doc.data().AadharCardNumber || '',
+          Address: doc.data().Address || '',
+          ContactNumber: doc.data().ContactNumber || '',
+          EmailID: doc.data().EmailId || '',
+          OrganizationName: doc.data().OrganizationName || '',
+        }));
+
+        setStudentApplications(studentData);
+      } catch (error) {
+        console.error('Error fetching student applications:', error);
+      }
+    };
+
+    fetchStudentApplications();
+  }, []);
+
+  const handleViewClick = (student) => {
+    setSelectedStudent(student);
+    setModalVisible(true);
+  };
+
+  const handleModalCancel = () => {
+    setModalVisible(false);
+    setSelectedStudent(null);
+  };
+
+  return (
+    <div>
+      <h2>Student Applications</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>S.No</th>
+            <th>Name</th>
+            <th>Disability Type</th>
+            <th>UDID</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {studentApplications.map((student, index) => (
+            <tr key={student.id}>
+              <td>{index + 1}</td>
+              <td>{student.Name}</td>
+              <td>{student.DisabilityType}</td>
+              <td>{student.UDID}</td>
+              <td>
+                <button
+                  className="view-button"
+                  onClick={() => handleViewClick(student)}
+                >
+                  View
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Modal
+        title="Student Application Details"
+        visible={modalVisible}
+        onCancel={handleModalCancel}
+        footer={null}
+      >
+        {selectedStudent && (
+          <div>
+            <p><strong>Name:</strong> {selectedStudent.Name}</p>
+            <p><strong>Disability Type:</strong> {selectedStudent.DisabilityType}</p>
+            <p><strong>UDID:</strong> {selectedStudent.UDID}</p>
+            {/* Add other fields as needed */}
+            <p><strong>Aadhar Card Number:</strong> {selectedStudent.AadharCardNumber}</p>
+            <p><strong>Address:</strong> {selectedStudent.Address}</p>
+            <p><strong>Contact Number:</strong> {selectedStudent.ContactNumber}</p>
+            <p><strong>Email ID:</strong> {selectedStudent.EmailID}</p>
+            <p><strong>Organization Name:</strong> {selectedStudent.OrganizationName}</p>
+
+            {/* Add "Accept" and "Decline" buttons */}
+            <Button type="primary">Accept</Button>
+            <Button type="danger">Decline</Button>
+          </div>
+        )}
+      </Modal>
+    </div>
+  );
+};
+
+export default DisplayStudentApplications;
